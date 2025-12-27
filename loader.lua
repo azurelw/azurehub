48dccf981303e6c61c241a3cb920ed860b46b950
azurelw <yupio2acxount@gmail.com>
2025-12-28 03:24:48 +0500

    Update script URL in loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ebdffb9ac953705b47da2649005fe72d07e80363a4dbf469bfbcf4680711c22a/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/f4a3b2aaaa6af3c36005d793f351f56b6c0fd08002db92591522b9f723688b8c/download"))()
